40da36c09c0424efdbbf05879f6df1bf736f5f5b
Pedro Paulo <pedropaulotj@gmail.com>
2021-09-19 18:57:09 -0300

    fix: logo deploy vercel

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,9 @@ export function Header() {
   return (
     <header className="header">
       <div>
-        <img src="/logo.svg" alt="to.do"/>
+        {/* <img src="/logo.svg" alt="to.do"/> */}
+        {/* Coloquei assim para buscar no deploy da Vercel, o certo é o de cima*/}
+        <img src="https://raw.githubusercontent.com/pedropaulodf/ignite-chapter1-desafio-principal/main/public/logo.svg" alt="to.do"/>
       </div>
     </header>
   )
